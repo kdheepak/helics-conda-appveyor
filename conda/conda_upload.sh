ea1476c106b17d11264078e65f3d5e338e68be02
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-27 17:20:34 -0600

    Fix build string for upload

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -8,4 +8,4 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION="1.2.1"
 conda build helics
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER $CONDA_BLD_PATH/$OS/$PKG_NAME-*.tar.bz2 --force
