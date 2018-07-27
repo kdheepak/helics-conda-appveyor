cff6bde568929965fde4e5f923527f30c32f601f
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-27 17:02:51 -0600

    Update conda_install.sh

--- conda/conda_install.sh
+++ conda/conda_install.sh
@@ -5,9 +5,8 @@ set -ev # exit on first error, print each command
 if [ ${PYTHON_ARCH} == "32" ]; then
   set CONDA_FORCE_32BIT=1
 fi
-conda create -q -n testenv ${DEPS} python=${PYTHON_VERSION}
-source activate testenv
+conda create -q -n helics-build-environment python=$PYTHON_VERSION
+source activate helics-build-environment
 conda info -a
 conda list
-conda create -q -n helics-build-environment python=$TRAVIS_PYTHON_VERSION
 
