85b0b9c92e246f39450455d9e46c9bb8d15772ee
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-28 09:06:18 -0600

    Remove homebrew after downloading conda

--- conda/conda_setup.sh
+++ conda/conda_setup.sh
@@ -2,15 +2,6 @@
 
 set -ev # exit on first error, print each command
 
-if [ ${TRAVIS_OS_NAME} == "osx" ]; then
-    echo ""
-    echo "Removing homebrew from Travis CI to avoid conflicts."
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall > ~/uninstall_homebrew
-    chmod +x ~/uninstall_homebrew
-    ~/uninstall_homebrew -fq
-    rm ~/uninstall_homebrew
-fi
-
 MINICONDA_URL="http://repo.continuum.io/miniconda"
 
 if [ ${PYTHON_VERSION:0:1} == "2" ]; then
@@ -29,6 +20,16 @@ else
     URL="${MINICONDA_URL}/${MINICONDA}-latest-${MINICONDA_OS}-x86.sh"
 fi
 travis_retry wget "${URL}" -O miniconda.sh
+
+if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+    echo ""
+    echo "Removing homebrew from Travis CI to avoid conflicts."
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall > ~/uninstall_homebrew
+    chmod +x ~/uninstall_homebrew
+    ~/uninstall_homebrew -fq
+    rm ~/uninstall_homebrew
+fi
+
 chmod +x miniconda.sh
 ./miniconda.sh -b -p "${HOME}/miniconda"
 export PATH="${HOME}/miniconda/bin:${PATH}"
